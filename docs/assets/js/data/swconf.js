06981f3ec83e7d28c449e2f288b7c3b50e7c74ba
infominer33 <nfo@infominer.xyz>
2024-11-22 05:40:17 -0500

    chore: change destination

---
+++ docs/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1732271973',resources: [
+      '/https://infominer33.github.io/assets/css/jekyll-theme-chirpy.css',
+      '/https://infominer33.github.io/',
+      
+        '/https://infominer33.github.io/categories/',
+      
+        '/https://infominer33.github.io/tags/',
+      
+        '/https://infominer33.github.io/archives/',
+      
+        '/https://infominer33.github.io/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
