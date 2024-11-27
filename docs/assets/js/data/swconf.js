8a78e929e86b9bdd3c21f3044229753d26b0fe98
infominer33 <nfo@infominer.xyz>
2024-11-27 13:42:53 -0500

    build

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732715818',resources: [
+    cacheName: 'chirpy-1732732938',resources: [
       '/github-discussions/assets/css/jekyll-theme-chirpy.css',
       '/github-discussions/',
       
